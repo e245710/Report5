0024921d38abb0c5edfa39132fd85192efb68c1b
Haruto Yamauchi <e245710@ie.u-ryukyu.ac.jp>
2024-12-17 01:16:49 +0900

    3rdcommit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    
-        String str = null; // nullを代入
-        try {
-            System.out.println(str.length()); // NullPointerExceptionが発生
-        } catch (NullPointerException e) { // NullPointerExceptionをキャッチ
-            System.out.println("NullPointerExceptionが発生しました！"); // メッセージ出力
-            System.out.println("例外メッセージ: " + e.getMessage()); // 例外詳細出力
-        }
-    }
-}
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
+    }    
+}    
